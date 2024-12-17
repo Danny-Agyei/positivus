156a2ececf350fd850faf7b31e667f64c2fce205
Daniel Agyei <dandesign96@gmail.com>
2024-12-17 19:13:05 +0000

    @responsiveness: done with large screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
   const navToggleBtn = $(".js-toggle");
   const accordionBtns = $(".js-accordion-toggle");
   const navbar = $(".js-nav");
-  const allCarousels = $(".js-carousel");
+  const allSliders = $(".js-slider");
+  const caseSlider = $(".js-case-slider");
+  const testOwl = $(".js-testimonials-slider");
+  const testOwlDot = $(".js-testimonails-dot");
 
   // Navbar mobile menu toggler
   const MENU__BTN_ACTIVE_CLASS = "header__menu--active";
@@ -31,78 +34,96 @@ $(document).ready(function () {
   }
 
   // Owl carousel config
-  const screenWidth = $(window).width();
-
-  [...allCarousels].forEach((elem) => {
-    const sectionName = $(elem).attr("data-name");
-
-    const options = {
-      case: {
-        responsiveClass: true,
-        autoPlayTimeout: 1000,
-        autoplaySpeed: 1800,
-        smartSpeed: 800,
-        margin: 0,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        responsive: {
-          768: {
-            margin: 10,
-            items: 2,
-          },
-          992: {
-            item: 1,
-            margin: 0,
-            autoplay: false,
-          },
-        },
-      },
-      testimonials: {
-        responsiveClass: true,
-        autoPlayTimeout: 1000,
-        autoplaySpeed: 1800,
-        smartSpeed: 800,
-        autoplay: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: [
-          '<i class="fa fas-left-arrow" aria-hidden="true"></i>',
-          '<i class="fa fas-right-arrow" aria-hidden="true"></i>',
-        ],
-        responsive: {
-          768: {
-            nav: true,
-            items: 2,
-            margin: 50,
-          },
-          992: {
-            nav: true,
-            items: 2,
-            margin: 50,
-          },
-        },
-      },
-    };
-
-    let filteredOptions = { ...options[sectionName] };
-
-    // Disable Case carousel on large screen
-    if (screenWidth > 990) {
-      const caseCarousel = $(".case__carousel");
-
-      caseCarousel.trigger("destroy.owl.carousel");
-      filteredOptions = { ...options["testimonials"] };
-    }
-
-    $(elem).owlCarousel({
-      center: true,
-      items: 1,
+  const options = {
+    case: {
+      responsiveClass: true,
+      autoPlayTimeout: 1000,
+      autoplaySpeed: 1800,
+      smartSpeed: 800,
+      margin: 0,
       loop: true,
-      autoHeight: true,
-      autoplayHoverPause: true,
-      ...filteredOptions,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      responsive: {
+        768: {
+          margin: 10,
+          items: 2,
+        },
+        992: {
+          item: 1,
+          margin: 0,
+          autoplay: false,
+        },
+      },
+    },
+    testimonials: {
+      responsiveClass: true,
+      autoPlayTimeout: 1000,
+      autoplaySpeed: 1800,
+      smartSpeed: 800,
+      autoplay: false,
+      margin: 0,
+      loop: false,
+      nav: true,
+      dots: true,
+      navText: [
+        '<i class="fa-solid fa-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa-solid fa-arrow-right" aria-hidden="true"></i>',
+      ],
+      responsive: {
+        768: {
+          items: 2,
+          margin: 50,
+        },
+        992: {
+          items: 2,
+          margin: 50,
+        },
+      },
+    },
+  };
+
+  $(window).resize(startCarousel);
+
+  startCarousel();
+
+  function startCarousel() {
+    const screenWidth = $(window).width();
+
+    [...allSliders].forEach((elem) => {
+      const sectionName = $(elem).attr("data-name");
+      let availableCarousel = { ...options[sectionName] };
+
+      // Disable Case carousel on large screen
+      if (screenWidth > 990 && caseSlider.hasClass("owl-carousel")) {
+        caseSlider.trigger("destroy.owl.carousel");
+        availableCarousel = { ...options["testimonials"] };
+      }
+
+      $(elem).owlCarousel({
+        center: true,
+        items: 1,
+        autoHeight: true,
+        autoplayHoverPause: true,
+        ...availableCarousel,
+      });
+    });
+  }
+
+  // Handle custom carousel dots trigger
+  testOwlDot.click(function () {
+    testOwl.trigger("to.owl.carousel", [$(this).index()]);
+  });
+
+  // Add active class to next custom dot
+  testOwl.on("changed.owl.carousel", function (event) {
+    let index = event.page.index;
+
+    testOwlDot.removeClass("active");
+
+    testOwlDot.each(() => {
+      testOwlDot.eq(index).addClass("active");
     });
   });
 });
